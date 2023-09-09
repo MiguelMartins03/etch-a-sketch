639089c11e5f09cf5a17bfa5efe26d25917fac9a
Miguel Martins <miguel.vale.martins@gmail.com>
2023-09-09 18:13:02 +0100

    Implement a 16x16 div grid

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+for(let i = 0; i < 16; i++){
+    const row = document.createElement("div");
+    row.classList.add("row", `${i}`);
+    container.appendChild(row);
+    for(let j = 0; j < 16; j++){
+        const collumn = document.createElement("div");
+        collumn.classList.add("collumn", `${j}`);
+        row.appendChild(collumn);
+    }
+}
